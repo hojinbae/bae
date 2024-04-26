8b6f6c7a9690f90b543b020f95e05be8ad07cfdf
hojinbae <153696184+hojinbae@users.noreply.github.com>
2024-04-26 20:49:48 +0900

    ho

--- src/routes/personalized.js
+++ src/routes/personalized.js
@@ -10,116 +10,54 @@ import  {useNavigate, useParams} from "react-router-dom";
 
     return (
         <div className={styles.mainContainer} >
-            <div className={styles.title}>
-                <h2> GARAGE 추천 축제 </h2>
+
+            <div className={styles.recommend}>
+                <div className={styles.recommend_title_container}>
+                    <div className={styles.recommend_main_title}> GARAGE 추천 축제</div>
+                    <div className={styles.recommend_sub_title}> GARAGE에서 추천하는 축제입니다.</div>
+                </div>
+                <div className={styles.recommend_button_container}>
+                    <div className={styles.recommend_button}><button> 계절 </button></div>
+                    <div className={styles.recommend_button}><button> 지역 </button></div>
+                    <div className={styles.recommend_button}><button> 세트 </button></div>
+                </div>
+                <div className={styles.recommend_boxes_container}>
+                    <div className={styles.recommend_box}>첫 번째 박스</div>
+                    <div className={styles.recommend_box}>두 번째 박스</div>
+                    <div className={styles.recommend_box}>세 번째 박스</div>
+                </div>
             </div>
-            <div className={styles.firstContainer}>
-                <div className={styles.recommend}>
-                        <div className={styles.recommendCategory1}>
-                        {/*    카테고리별 추천(맟춤추천)*/}
-                            {/*<div className={styles.recommendCategory1}>*/}
-                                {/*추천지역축제콘텐츠*/}
-                                {/*<button*/}
-                                {/*    // onClick={() => setCurrentIndex(currentIndex => (currentIndex - 1 + items.length) % items.length)}*/}
-                                {/*    // className={styles.controlBtn}>&lt;</button>*/}
-                                <div className={styles.recommendButton}>
-                                    {/*<div>{items[currentIndex].main}</div>*/}
-                                    {/*<div>{items[currentIndex].text}</di|v>*/}
-                                    <button> 계절 </button>
-                                </div>
-                                <div className={styles.recommendButton}>
-                                    <button> 지역 </button>
-                                </div>
-                                <div className={styles.recommendButton}>
-                                    <button> 세트 </button>
-                                </div>
-                            {/*</div>*/}
-                        </div>
-                    <div className={styles.recommendCategory2}>
-                    {/*    지역 우선*/}
-                    {/*    <div className={styles.recommendCategory2}>*/}
-                    {/*        /!*지역우선콘텐츠*!/*/}
-                                <div className={styles.recommendContent1}>
-                                    내용1
-                                    {/*<div className={styles.recommendContent2}>*/}
-                                    {/*    내용*/}
-                                    {/*</div>*/}
-                                </div>
-                                <div className={styles.recommendContent1}>
-                                    내용2
-                                    {/*<div className={styles.recommendContent2}>*/}
-                                    {/*    내용*/}
-                                    {/*</div>*/}
-                                </div>
-                                <div className={styles.recommendContent1}>
-                                    내용3
-                                    {/*<div className={styles.recommendContent2}>*/}
-                                    {/*    내용*/}
-                                    {/*</div>*/}
-                                </div>
-                        {/*</div>*/}
+
+            <div className={styles.custom}>
+                <div className={styles.custom_title_container}>
+                    <div className={styles.custom_title}>맞춤 여행지</div>
+                    <div className={styles.custom_sub_title}>성향에 따른 맞춤형 여행지를 추천해 드립니다.</div>
+                    <div className={styles.custom_button}><button> 다시 추천 받기 </button></div>
+                </div>
+                <div className={styles.custom_boxes_container}>
+                    <div className={styles.custom_box}>랜덤 박스 1</div>
+                    <div className={styles.custom_box}>랜덤 박스 2</div>
+                    <div className={styles.custom_box}>랜덤 박스 3</div>
+                    <div className={styles.custom_box}>랜덤 박스 4</div>
+                </div>
+            </div>
+
+            <div className={styles.chart}>
+                <div className={styles.traffic_chart_title_container}>
+                    교통별 차트
+                    <div className={styles.traffic_chart}>
+                       차트1
+                    </div>
+                </div>
+                <div className={styles.search_chart_title_container}>
+                    연령별 검색량 차트
+                    <div className={styles.search_chart}>
+                        차트2
                     </div>
                 </div>
             </div>
-            <div className={styles.secondContainer}>
-                {/*<div className={styles.traffic}>*/}
-                {/*    교통량*/}
-                {/*    <div className={styles.secondContainerContent}>*/}
-                {/*        교통량내용*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-            {/*</div>*/}
-            {/*<div className={styles.SearchByAgeContainer}>*/}
-            {/*    <div className={styles.SearchByAge}>*/}
-            {/*        연령별 검색*/}
-            {/*        <div className={styles.secondContainerContent}>*/}
-            {/*            연령별 검색 내용*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-                <div className={styles.RandomContainer}>
-                    <div className={styles.RandomContent}>
-                        랜덤1
-                    </div>
-                    <div className={styles.RandomContent}>
-                        랜덤2
-                    </div>
-                    <div className={styles.RandomContent}>
-                        랜덤3
-                    </div>
-                    <div className={styles.RandomContent}>
-                        랜덤4
-                    </div>
-                </div>
-            </div>
-            <div>
-            <div className={styles.thirdContainer}>
-                여기에 차트
-                {/*<div className={styles.RestaurantMain}>*/}
-                {/*    인기식당 맞춤*/}
-                {/*    <div className={styles.RestaurantCategory}>*/}
-                {/*        <div className={styles.RestaurantContent}>*/}
-                {/*        주차장 넓은, 꽃 많은 카페*/}
-                {/*        </div>*/}
-                {/*        <div className={styles.RestaurantContent}>*/}
-                {/*            놀이방*/}
-                {/*        </div>*/}
-                {/*        <div className={styles.RestaurantContent}>*/}
-                {/*            가성비*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-            </div>
-            </div>
-
-
-
-
-
-
-
-
         </div>
     )
-}
+ }
 
 export default Personalized;
